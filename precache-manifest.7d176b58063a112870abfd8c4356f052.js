d51f8bc5f323fa8144d39e8887c8893e4f6b258d
Алексей Чупров <alexey.chuprov@gmail.com>
2020-02-14 03:47:10 +0300

    Updates

---
+++ precache-manifest.7d176b58063a112870abfd8c4356f052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5014a52b96a35783e80edc33356da738",
+    "url": "/cra-ts/index.html"
+  },
+  {
+    "revision": "102fa2e1871f47347d7a",
+    "url": "/cra-ts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "323ff73aa7f9e8c4efcf",
+    "url": "/cra-ts/static/js/2.c7a2249f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-ts/static/js/2.c7a2249f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102fa2e1871f47347d7a",
+    "url": "/cra-ts/static/js/main.4eba8564.chunk.js"
+  },
+  {
+    "revision": "23145bd5e61f1667d59b",
+    "url": "/cra-ts/static/js/runtime-main.c693a2a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-ts/static/media/logo.5d5d9eef.svg"
+  }
+]);
